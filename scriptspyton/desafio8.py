98c8dbb887ea36edf011f45d64d8d20b6ca3c013
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-10 22:26:12 +0000

    desafio calcular centimetros e milimetros

---
+++ scriptspyton/desafio8.py
@@ -0,0 +1,5 @@
+print('Programa Calcula Centimetros e Milimetros')
+
+num = int(input('Digite o valor em metros: '))
+
+print('O valor em centímetros é: {}\nO valor em milímetros é: {}'.format(num*100, num*1000))
