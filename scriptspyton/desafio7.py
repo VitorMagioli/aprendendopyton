f022d511e48dba7b67087140dd63e4f1361cd558
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-10 22:21:15 +0000

    desafio calcula media do aluno

---
+++ scriptspyton/desafio7.py
@@ -0,0 +1,6 @@
+print('Programa Calcular Media do Aluno')
+
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota:'))
+
+print('A média do aluno é {}'.format((n1+n2)/2))
