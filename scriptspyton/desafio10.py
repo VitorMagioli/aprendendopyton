e36e21e1ec4d6ab4096c5bb4982ccdd7af3aa47f
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-10 22:54:58 +0000

    alteração cotação dólar atualizada

--- scriptspyton/desafio10.py
+++ scriptspyton/desafio10.py
@@ -2,4 +2,4 @@ print('Compra de Dólar')
 
 real = int(input('Digite quantos reais você tem: '))
 
-print('Você pode comprar {:.2f} dólares.'.format(real/3.27))
+print('Você pode comprar {:.2f} dólares.'.format(real/5.84))
