e04cbd2adfa1172bee6f069a382067eb5b643440
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-11 14:36:22 +0000

    desafio 22 aula 9

---
+++ scriptspyton/desafio14.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome:')
+
+print(nome.upper())
+print(nome.lower())
+print(nome.strip())
+print(len(nome))
+dividido = nome.split()
+print(dividido[0])
+print(len(dividido[0]))
