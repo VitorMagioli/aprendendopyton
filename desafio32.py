dcb07ac212367e50e0f2701e37a96ae315c91272
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-13 21:04:46 +0000

    cálculo ano bissexto (resto da divisão)

---
+++ desafio32.py
@@ -0,0 +1,8 @@
+print('           Descobrindo Ano Bissexto           ')
+
+ano = int(input('Digite o ano: '))
+
+if ano % 4 == 0:
+    print('O ano é bissexto!')
+else:
+    print('O ano não é bissexto!')
