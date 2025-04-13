dabd47a58161819f8d7c777782a873516e6117a1
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-13 22:02:32 +0000

    inserção do AND e do OR na condição IF para ficar completo o exercício

--- desafio32.py
+++ desafio32.py
@@ -2,7 +2,7 @@ print('           Descobrindo Ano Bissexto           ')
 
 ano = int(input('Digite o ano: '))
 
-if ano % 4 == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('{} é bissexto!'.format(ano))
 else:
     print('{} não é bissexto!'.format(ano))
