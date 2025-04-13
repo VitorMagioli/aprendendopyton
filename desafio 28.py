8a09879b67a77b40e674540e2ac1f675337e26a1
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-13 16:16:21 +0000

    jogo de advinhação função if else

---
+++ desafio 28.py
@@ -0,0 +1,17 @@
+print('      Jogo de Advinhação     ')
+print('   Tente descobrir qual número o computador pensou entre 0 e 5.')
+
+num = int(input('Digite o número que você acha que é: '))
+
+if num == 0:
+    print('Você errou! Tente novamente')
+if num == 1:
+    print('Você errou! Tente novamente')
+if num == 2:
+    print('Você errou! Tente novamente')
+if num == 3:
+    print('Você acertou! PARABÉNS!!!!!')
+if num == 4:
+    print('Você errou! Tente novamente')
+if num == 5:
+    print('Você errou! Tente novamente')
