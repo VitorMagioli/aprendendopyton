b4c392b5d319c99e910924df25841df310c5b48b
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-13 22:31:18 +0000

    correção da lógica do programa de OR para AND

--- desafio35.py
+++ desafio35.py
@@ -4,7 +4,8 @@ r1 = int(input('Digite o valor da primeira reta(em cm): '))
 r2 = int(input('Digite o valor da segunda reta(em cm): '))
 r3 = int(input('Digite o valor da terceira reta(em cm:) '))
 
-if (r1+r2) > r3 or (r2+r3) > r1 or (r1+r3) > r2:
+if r1+r2 > r3 and r2+r3 > r1 and r1+r3 > r2:
     print('É possível formar um triângulo com essas medidas!')
 else:
     print('não é possível formar um triângulo com essas medidas.')
+    
