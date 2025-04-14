69299bfda390a07bc2490bd7acceda9818524833
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 14:48:10 +0000

    inserção de comentários

--- desafio36.py
+++ desafio36.py
@@ -1,9 +1,16 @@
+print('-=-'*20)
+print('SISTEMA DE FINANCIAMENTO BANCÁRIO - CASAS')
+print('-=-'*20)
+
+#Solicitação para preenchimento do usuário
 valCasa = float(input('Valor da Casa:R$ '))
 salComprador = float(input('Salário do Comprador:R$ '))
 anos = int(input('Quantos anos? '))
 
+#Convertendo os anos em meses para calculo da parcela mensal
 meses = anos * 12
 
+#Cálculo do valor da prestação mensal
 prestacao = valCasa / meses
 
 if prestacao > ((salComprador*30)/100):
