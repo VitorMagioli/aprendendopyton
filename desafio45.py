3d5fca49bfbb7f94eaa292c73d4b4e8f3ab8b6bd
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 23:18:00 +0000

    corrigindo as condições

--- desafio45.py
+++ desafio45.py
@@ -1,6 +1,6 @@
 from random import randint
 from time import sleep
-computador = str(randint(0, 2)) #três possibilidades
+computador = randint(1, 3) #três possibilidades
 print('-=-'*20)
 print('      !!!! JOKENPO !!!!     ')
 print('-=-'*20)
@@ -25,42 +25,30 @@ elif escolha == 1 and computador == 2:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[PEDRA] Parabéns! PEDRA vence TESOURA. Você venceu!!!\n')
-    computador.replace(computador, 'TESOURA')
-    print('Eu escolhi... {}'.format(computador))
 
 elif computador == 1 and escolha == 2:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[PEDRA] Você perdeu! PEDRA vence TESOURA. Computador venceu!!!\n')
-    computador.replace(computador, 'PEDRA')
-    print('Eu escolhi... {}'.format(computador))
 
 elif escolha == 2 and computador == 3:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[TESOURA] Parabéns! TESOURA vence PAPEL. Você venceu!!!\n')
-    computador.replace(computador, 'PAPEL')
-    print('Eu escolhi... {}'.format(computador))
 
 elif computador == 2 and escolha == 3:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[TESOURA] Você perdeu! TESOURA vence PAPEL. Computador venceu!!!\n')
-    computador.replace(computador, 'TESOURA')
-    print('Eu escolhi... {}'.format(computador))
-    
+
 elif escolha == 1 and computador == 3:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[PAPEL] Parabéns! PAPEL vence PEDRA. Você venceu!!!\n')
-    computador.replace(computador, 'PAPEL')
-    print('Eu escolhi... {}'.format(computador))
 
 elif computador == 1 and escolha == 3:
     print('JO - KEN - PO.....\n')
     sleep(1)
     print('[PAPEL] Você perdeu! PAPEL vence PEDRA. Computador venceu!!!\n')
-    computador.replace(computador, 'PEDRA')
-    print('Eu escolhi... {}'.format(computador))
 
 print('Obrigado por Jogar!')
