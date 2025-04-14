030c8232132476e24147cc663d4e5ed24be6bbae
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 22:00:12 +0000

    inserção da condição IF aninhada para avaliar o tipo de triângulo

---
+++ desafio42.py
@@ -0,0 +1,21 @@
+print('-=-'*20)
+print('FORMANDO UM TRIÂNGULO')
+print('-=-'*20)
+
+r1 = int(input('Digite o valor da primeira reta(em cm): '))
+r2 = int(input('Digite o valor da segunda reta(em cm): '))
+r3 = int(input('Digite o valor da terceira reta(em cm:) '))
+
+if r1+r2 > r3 and r2+r3 > r1 and r1+r3 > r2:
+    print('É possível formar um triângulo com essas medidas!')
+    
+    #avaliando tipo de triângulo formado
+    if r1 == r2 and r1 == r3:
+        print('Equilátero')
+    elif r1 == r2 or r1 == r3:
+        print('Isósceles')
+    elif r1 != r2 and r1 != r3:
+        print ('Escaleno')
+
+else:
+    print('não é possível formar um triângulo com essas medidas.')
