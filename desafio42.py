5e28f69d092980805bcc6c56bd5417214df43e4c
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 22:02:56 +0000

    correção da lógica do ELIF "escaleno"

--- desafio42.py
+++ desafio42.py
@@ -14,7 +14,7 @@ if r1+r2 > r3 and r2+r3 > r1 and r1+r3 > r2:
         print('Equilátero')
     elif r1 == r2 or r1 == r3:
         print('Isósceles')
-    elif r1 != r2 and r1 != r3:
+    elif r1 != r2 and r1 != r3 and r2 != r3:
         print ('Escaleno')
 
 else:
