7875ce9be6b00eafbe8a016995e33cbb05e8d4cb
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 14:58:28 +0000

    renomeação do arquivo

---
+++ desafio37.py
@@ -0,0 +1,16 @@
+from time import sleep
+print('-=-'*20)
+print('SISTEMA DE CONVERSÃO DE BASES NUMÉRICAS')
+print('-=-'*20)
+
+print('Menu de Opções:')
+sleep(1)
+#Solicita escolha da base desejada
+escolha = (int(input('Digite 1 para converter em BINÁRIO.\nDigite 2 para converter em OCTAL.\nDigite 3 para converter em HEXADECIMAL.')))
+
+if escolha == 1:
+    num = int(input('Digite o valor que você deseja converter: '))
+    resto = 0
+    resto += num % 2
+    if ()
+    print('')
