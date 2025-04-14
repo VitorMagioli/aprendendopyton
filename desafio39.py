fe004a7b92045b87fdcb761198ab1fbec99f7cab
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-14 15:23:07 +0000

    importação da biblioteca e if funcionando

--- desafio39.py
+++ desafio39.py
@@ -12,8 +12,9 @@ id = hoje - nasc
 
 if id > 18:
     print('Já passou o tempo de alistamento!')
+    print('Passou {} anos do prazo para alistamento. Se você não se apresentou procure a Junta de Serviço Militar.'.format(id-18))
 elif id > 17:
     print('Está na hora de se alitar! Procure a Junta de Serviço Militar.')
 else:
     print('Ainda não chegou o momento de se alistar.')
-    
+    print('Faltam {} anos para você se alistar.'.format(18 - id))
