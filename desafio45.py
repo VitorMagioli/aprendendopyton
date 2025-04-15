8c61e4cf9a024548e6f0014a2bf9a0193b5eb3cd
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-15 12:16:30 +0000

    alt formatação

--- desafio45.py
+++ desafio45.py
@@ -1,16 +1,24 @@
 from random import randint
 from time import sleep
-item = ('Pedra' , 'Tesoura', 'Papel')
+
+item = ('Pedra' , 'Tesoura', 'Papel') #para aparecer na tela a escolha do computador
 computador = randint(0, 2) #três possibilidades
+
 print('-=-'*20)
 print('      !!!! JOKENPO !!!!     ')
 print('-=-'*20)
+
+sleep(1)
+
 print('Escolha entre PEDRA, PAPEL e TESOURA e tente ganhar de mim!')
 
+sleep(1)
+
 print('''
-[0] PEDRA
-[1] TESOURA
-[2] PAPEL ''')
+[0] PEDRA\n
+[1] TESOURA\n
+[2] PAPEL \n
+''')
 
 escolha = int(input('Selecione para Jogar: '))
 
@@ -18,46 +26,74 @@ print ('PROCESSANDO...')
 sleep(2)
 
 if escolha == computador:
-    print('JO - KEN - PO.....')
+    print('JO')
     sleep(1)
-    print('EMPATE!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\nEMPATE!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 elif escolha == 0 and computador == 1:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[PEDRA] Parabéns! PEDRA vence TESOURA. Você venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[PEDRA] Parabéns! PEDRA vence TESOURA. Você venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 elif computador == 0 and escolha == 1:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[PEDRA] Você perdeu! PEDRA vence TESOURA. Computador venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[PEDRA] Você perdeu! PEDRA vence TESOURA. Computador venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 elif escolha == 1 and computador == 2:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[TESOURA] Parabéns! TESOURA vence PAPEL. Você venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[TESOURA] Parabéns! TESOURA vence PAPEL. Você venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 elif computador == 1 and escolha == 2:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[TESOURA] Você perdeu! TESOURA vence PAPEL. Computador venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[TESOURA] Você perdeu! TESOURA vence PAPEL. Computador venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
     
 elif escolha == 0 and computador == 2:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[PAPEL] Parabéns! PAPEL vence PEDRA. Você venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[PAPEL] Parabéns! PAPEL vence PEDRA. Você venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 elif computador == 0 and escolha == 2:
-    print('JO - KEN - PO.....\n')
+    print('JO')
     sleep(1)
-    print('[PAPEL] Você perdeu! PAPEL vence PEDRA. Computador venceu!!!\n')
-    print('O computador escolheu {}.'.format(item[computador]))
+    print('KEN')
+    sleep(1)
+    print('PO')
+    sleep(1)
+    print('\n[PAPEL] Você perdeu! PAPEL vence PEDRA. Computador venceu!!!\n')
+    print('O computador escolheu {}.\n'.format(item[computador]))
 
 else:
     print('[OPÇÃO INVÁLIDA!] Escolha uma das opções acima.')
