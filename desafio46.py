ff36076bda844c207daab1252cf0979368ca8105
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-15 17:49:28 +0000

    implementação do loop for

---
+++ desafio46.py
@@ -0,0 +1,12 @@
+from time import sleep
+print('-=-'*20)
+print('             CONTAGEM REGRESSIVA     ')
+print('-=-'*20)
+
+c = 0
+
+for c in range (10, 0, -1):
+    sleep(1)
+    print(c)
+sleep(1)
+print('Feliz ano novo!!!!!')
