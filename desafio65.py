7fe974c6af8686af3b4e6254a58a5d8796084d87
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-20 14:08:34 +0000

    inserindo a condição de maior e menor

--- desafio65.py
+++ desafio65.py
@@ -1,12 +1,20 @@
 r = 'S'
-q = s = 0
+q = s = maior = menor = 0
 while r in 'Ss':
     n = int(input('Digite um número: '))
     r = str(input('Quer continuar? [S/N]')).upper().strip()[0]
     s += n
     q += 1
+    if q == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
 
 m = s / q
 print('Você digitou {} números.'.format(q))
-print('A média dos números digitados é: {}'.format(m))
+print('A média dos números digitados é: {:.2f}'.format(m))
+print('O maior número é {} e o menor número é {}'.format(maior, menor))
 print('FIM')
