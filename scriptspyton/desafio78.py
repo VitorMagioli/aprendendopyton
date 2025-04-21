f02db38fd484950fdf8ae85192e89e03d84df481
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-21 22:11:02 +0000

    comentários

--- scriptspyton/desafio78.py
+++ scriptspyton/desafio78.py
@@ -5,12 +5,14 @@ for c in range(5):
 print('==='*20)
 print(f'Você digitou os valores {valores}')
 
+#PARA LER O MAIOR VALOR COM O PARÂMETRO MAX
 print(f'O maior valor digitado foi {max(valores)} nas posições ', end='')
 for v in range(0, len(valores)):
     if valores[v] == max(valores):
         print(f'{v}...', end='')
 
 print()
+#PARA LER O MENOR VALOR COM O PARÂMETRO MIN
 print(f'O menor valor digitado foi {min(valores)} nas posições ', end='')
 for v in range(0, len(valores)):
     if valores[v] == min(valores):
