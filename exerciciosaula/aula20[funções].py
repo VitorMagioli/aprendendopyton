83f9f44553eea86f72bf3615a06c0b6d5f018771
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-25 22:19:51 +0000

    calculadora com funções

--- exerciciosaula/aula20[funções].py
+++ exerciciosaula/aula20[funções].py
@@ -1,33 +1,36 @@
 def soma(a,b):
     s = a+b
-    print(s)
+    print(f'A soma entre {a} e {b} é igual a {s}')
 
 
 def sub(a, b):
     s = a - b
-    print(s)
+    print(f'A subtração entre {a} e {b} é igual a {s}')
 
 
 def mult(a, b):
     s = a * b
-    print(s)
+    print(f'A multiplicação entre {a} e {b} é igual a {s}')
 
 def div(a, b):
     s = a / b
-    print(s)
+    print(f'A divisão entre {a} e {b} é igual a {s}')
 
 def cab():
-    print('==='*10)
-    print('CALCULADORA')
-    print('==='*10)
+    print('=='*10)
+    print('     CALCULADORA')
+    print('=='*10)
+
 
 opcao = 0
 while True:
-    print('MENU: ')
-    print('[1] SOMA: ' \
-    '                 [2] SUBTRAÇÃO' \
-    '                 [3] MULTIPLICAÇÃO' \
-    '                 [4] DIVISÃO')
+    cab()
+    print('     MENU: ')
+    print('''    [1] SOMA 
+    [2] SUBTRAÇÃO
+    [3] MULTIPLICAÇÃO
+    [4] DIVISÃO''')
+    
     opcao = int(input('Escolha uma opção: '))
     if opcao == 1:
         a = int(input('1º número: '))
@@ -47,12 +50,7 @@ while True:
     if opcao == 4:
         a = int(input('1º número: '))
         b = int(input('2º número: '))
-        soma(a,b)
+        div(a,b)
         break
-    if opcao == 5:
-        a = int(input('1º número: '))
-        b = int(input('2º número: '))
-        soma(a,b)
-        break
-    if opcao > 5 or opcao < 0:
+    if opcao > 4 or opcao < 0:
         print('[OPÇÃO INVÁLIDA] Escolha uma das opções abaixo.')
