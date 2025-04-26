efbecc45b669073ac37217a5aa02e9569452b60c
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-26 20:01:16 +0000

    inclusão de interactive help, parametros opcionais, variavel de escopo local e escopo global

--- scriptspyton/desafio98.py
+++ scriptspyton/desafio98.py
@@ -1,13 +1,22 @@
 from time import sleep
 
-def contador (inicio, fim, passo):
+def contador (inicio=0, fim=0, passo=0): #PARÂMETROS OPCIONAIS
+    #INTERACTIVE HELP
+    """
+    - faz uma contagem e mostra na tela.
+    :param inicio: inicio da contagem
+    :param fim: fim da contagem
+    :param passo: passo da contagem
+    :return: sem retorno
+    :Função criada por Vitor Magioli
+    """
     print(f'Contagem de {inicio} até {fim} de {passo} em {passo}.')
     if passo < 0:
         passo *= -1
     if passo == 0:
         passo = 1
     if inicio < fim:
-        c = inicio
+        c = inicio #VARIÁVEL DE ESCOPO LOCAL
         while c <= fim:
             print(f'{c} ', end='', flush=True)
             sleep(0.5)
@@ -33,12 +42,15 @@ def contador (inicio, fim, passo):
 def lin():
     print('==='*30)
 
+help(contador)
+#PROGRAMA PRINCIPAL
 lin()
 contador(1, 10, 1)
 lin()
 contador(10, 0, 2)
 lin()
 print('Agora é sua vez de personalizar a contagem!')
+#VARIÁVEIS DE ESCOPO GLOBAL
 i = int(input('Início: '))
 f = int(input('Fim: '))
 p = int(input('Passo: '))
