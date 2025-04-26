cbcfa28bd2b80c92e6cc484b6a060a442aa41ba0
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-26 01:40:01 +0000

    tamanho adaptavel do cabeçalho

---
+++ scriptspyton/desafio97.py
@@ -0,0 +1,15 @@
+def escreva(msg):
+    tam = len(msg)
+    print('~'*tam)
+    print(msg)
+    print('~'*tam)
+
+escreva('Vitor Magioli')
+
+escreva('Lana Amaral')
+
+escreva('Ana Beatriz')
+
+escreva('Flamengo')
+
+escreva('Oi')
