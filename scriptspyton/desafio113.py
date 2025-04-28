e7e3c0d3b186340727fe698ac79e085d7952d6f3
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-28 15:25:57 +0000

    Funções leiaInt e leiaFloat com tratamento de exceção try except

---
+++ scriptspyton/desafio113.py
@@ -0,0 +1,37 @@
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        try:
+            n = str(input(msg))
+            n.isnumeric()
+            valor = int(n)
+            ok = True
+        except:
+            print('\33[0;31m[ERRO] Digite um número inteiro válido! \33[m')
+        if ok:
+            break
+    return valor
+
+
+def leiaFloat(msg):
+    ok = False
+    valor = 0
+    while True:
+        try:
+            n = str(input(msg))
+            n.isnumeric()
+            valor = float(n)
+            ok = True
+        except:
+            print('\33[0;31m[ERRO] Digite um número REAL válido! \33[m')
+        if ok:
+            break
+    return valor
+               
+
+#Programa Principal
+
+inteiro = leiaInt('Digite um número Inteiro: ')
+real = leiaFloat('Digite um número Real: ')
+print(f'Você digitou o número inteiro {inteiro} e o número real {real}.')
