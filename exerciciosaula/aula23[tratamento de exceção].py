c6ba9c861ed6016a5aacbc96074f573759a3ec61
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-28 15:06:21 +0000

    inclui a exceção keyboardinterrupt

--- exerciciosaula/aula23[tratamento de exceção].py
+++ exerciciosaula/aula23[tratamento de exceção].py
@@ -9,7 +9,7 @@ except (ValueError, TypeError):
     print('Tivemos um problema com o tipo de dado informado. Por favor, digite apenas números.')
 except ZeroDivisionError:
     print('Não é possível realizar a divisão por zero.')
-except:
+except KeyboardInterrupt:
     print('O usuário preferiu não informar os dados.')
 except Exception as erro:
     print(f'A causa do erro foi {erro.__cause__} da classe {erro.__class__}')
