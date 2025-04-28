4ab6000c7cf50f1c951c290512ee576e290677d1
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-28 00:00:33 +0000

    tratamento de erros

---
+++ exerciciosaula/aula23[tratamento de exceção].py
@@ -0,0 +1,20 @@
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    res = a / b
+
+#except Exception as erro:
+#    print(f'Problema encontrado: {erro.__class__}')
+except (ValueError, TypeError):
+    print('Tivemos um problema com o tipo de dado informado. Por favor, digite apenas números.')
+except ZeroDivisionError:
+    print('Não é possível realizar a divisão por zero.')
+except:
+    print('O usuário preferiu não informar os dados.')
+except Exception as erro:
+    print(f'A causa do erro foi {erro.__cause__} da classe {erro.__class__}')
+else:
+    print(f'O resultado é {res:.1f}')
+
+finally:
+    print('Obrigado! Volte sempre.')
